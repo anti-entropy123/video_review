ae9f514a63a75b2fe734af7b3761e864f29edbc7
anti-entropy123 <1348651580@qq.com>
2020-11-14 22:20:25 +0800

    完成ws接口(未测试)

--- back-end/server/app/ws/view.py
+++ back-end/server/app/ws/view.py
@@ -3,13 +3,13 @@ import time
 from app.utils import build_response
 from flask import request
 from flask_socketio import SocketIO, emit, join_room, leave_room
-from ..model import User, Meeting, Video
+from ..model import Comment, User, Meeting, Video
 from . import ws
 
 name_space = '/meetingRoom'
 
 meetingroom_manager = {}
-
+userId_manager = {}
 # 会议中的一个成员
 class MeetingMember:
     def __init__(self, user_id) -> None:
@@ -65,6 +65,7 @@ class VideoPlayer:
             'postion': self.get_postion(),
             'isPlay': self.get_isPlay()
         }
+        
 
 # 一个会议室
 class MeetingRoom:
@@ -104,6 +105,18 @@ class MeetingRoom:
                 'position': comment.postion
             })
         return result
+    
+    def add_comment(self, from_id, from_name, content, image_url, postion):
+        comment = Comment(
+            from_=from_id,
+            fromName=from_name,
+            postion=postion,
+            image=image_url,
+            content=content
+        )
+        video = Video.objects(url=self.player.url).first()
+        video.comment.append(comment)
+        video.save()
 
 @ws.on('connect', namespace=name_space)
 def connected_msg(data):
@@ -111,7 +124,11 @@ def connected_msg(data):
     meeting_id = data['meetingId']
     user_id = data['userId']
     
-    meeting_room: MeetingRoom = meetingroom_manager[meeting_id]
+    userId_manager[request.sid] = user_id
+    if meeting_id in meetingroom_manager:
+        meeting_room: MeetingRoom = meetingroom_manager[meeting_id]
+    else:
+        meeting_room = MeetingRoom(meeting_id=meeting_id)
     meeting_room.add_member(user_id=user_id)
     
     # 加入会议室room, 方便广播
@@ -124,14 +141,17 @@ def connected_msg(data):
         room=meeting_id
     )
     # 向新成员发送当前视频的播放
+    video_status = meeting_room.player.get_video_status()
+    video_status['reason'] = ''
+    video_status['userName'] = ''
     emit(
         'sycnVideoState', 
-        meeting_room.player.get_video_status()
+        build_response(data=video_status) 
     )
     # 向新成员发送当前批注的列表
     emit(
         'updateComment',
-        meeting_room.get_comment_list()
+        build_response(data=meeting_room.get_comment_list())
     )
 
 @ws.on('disconnect', namespace=name_space)
@@ -139,6 +159,7 @@ def disconnect_msg(data):
     meeting_id = data['meetingId']
     # print('client disconnected.')
     leave_room(meeting_id)
+    userId_manager.pop(request.sid)
 
 # @ws.on('my_event', namespace=name_space)
 # def test_message(message):
@@ -146,3 +167,83 @@ def disconnect_msg(data):
 #     emit('my_response',
 #          {'data': message['data'], 'count': 1})
 
+@ws.on('changeProcess', namespace=name_space)
+def controll_player(data):
+    type = data['type']
+    postion = data['postion']
+    url = data['url']
+    meeting_id = data['meetingId']
+    user = User.get_user_by_id(userId_manager[request.sid])
+
+    meeting_room: MeetingRoom = meetingroom_manager[meeting_id]
+    video_player: VideoPlayer = meeting_room.player
+    video_status = {
+        'userName': user.username,
+        'reason': type,
+    }
+    if type == 0 or type == 1:
+        video_player.pause()
+    elif type==2:
+        video_player.move_process(postion=postion)
+    elif type == 3 or type==4:
+        video_player.play()
+    elif type == 5:
+        video_player.change_video(url=url)
+    else:
+        raise RuntimeError('无效的type')
+
+    video_status.update(video_player.get_video_status())
+    ws.emit(
+        'sycnVideoState',
+        build_response(video_status),
+        room=meeting_id
+    )
+
+@ws.on('addComment', namespace=name_space)
+def addComment(data):
+    meeting_id = data['meetingId']
+    content = data['content']
+    image_url = data['imageUrl']
+    postion = data['postion']
+    from_id = userId_manager[request.sid]
+    user = User.get_user_by_id(from_id)
+    from_name = user.username
+
+    meeting_room: MeetingRoom = meetingroom_manager[meeting_id]
+    meeting_room.add_comment(
+        from_id=from_id,
+        from_name=from_name,
+        content=content,
+        image_url=image_url,
+        postion=postion
+    )
+    
+    emit(
+        'updateComment',
+        build_response(data=meeting_room.get_comment_list()),
+        room=meeting_id
+    )
+
+@ws.on('memberPermission', namespace=name_space)
+def setPermission(data):
+    target_id = data['userId']
+    meeting_id = data['meetingId']
+    meeting_room: MeetingRoom = meetingroom_manager[meeting_id]
+    user_id = userId_manager[request.sid]
+    if not user_id == meeting_room.manager_id:
+        return
+    target_user: MeetingMember = meeting_room.member_list[target_id]
+    
+    control = data.get('control', -1)
+    if control >= 0:
+        target_user.control = control
+
+    comment = data.get('comment', -1)
+    if comment >= 0:
+        target_user.comment = comment
+
+    ws.emit(
+        'sycnMember',
+        build_response(data=meeting_room.get_member_list()),
+        room=meeting_id
+    )
